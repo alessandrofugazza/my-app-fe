c2463bc00663a7d439bcdd457b07db6ad5980352
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-08-07 12:39:27 +0200

    definitely something significant

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,28 @@
-import { Col, Row } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 import HomeCardGrid from "../components/HomeCardGrid";
 
 export default function HomePage() {
   return (
-    <Row>
-      <Col>
-        <HomeCardGrid />
-      </Col>
-    </Row>
+    <div className="d-flex flex-column gap-5">
+      <Card>
+        <Card.Body>
+          <Card.Title className="display-1 fw-semibold">MOTD</Card.Title>
+          <Card.Text className="fs-2">
+            <span>
+              FUCK THIS LIFE MAN
+              <br />
+              THE DOORS ROCKS
+              <br />
+              THE END
+            </span>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Row>
+        <Col>
+          <HomeCardGrid />
+        </Col>
+      </Row>
+    </div>
   );
 }
