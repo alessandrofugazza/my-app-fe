faf90c7d59e9efb0405d48baff64d73a735bd224
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-08-08 23:57:30 +0200

    fixes an error and creates another one

--- src/pages/sources/components/SourceForm.tsx
+++ src/pages/sources/components/SourceForm.tsx
@@ -1,17 +1,31 @@
 import { useEffect, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
+interface SourceFormData {
+  title: string;
+  description: string;
+  author: string;
+  topic: string;
+  type: string;
+}
+
+interface SourceFormProps {
+  initialData: SourceFormData;
+  onSubmit: (formData: SourceFormData) => Promise<void>;
+}
+
 export default function SourceForm({
   initialData = { title: "", description: "", author: "", topic: "", type: "" },
   onSubmit,
-}) {
+}: SourceFormProps) {
   const [sourceFormData, setSourceFormData] = useState(initialData);
 
   useEffect(() => {
     setSourceFormData(initialData);
   }, [initialData]);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  // CHECK why did i have to add  | HTMLTextAreaElement
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
     const { name, value } = event.target;
     setSourceFormData({
       ...sourceFormData,
@@ -55,9 +69,11 @@ export default function SourceForm({
           onChange={handleInputChange}
           name="topic"
         >
+          {/* TODO fetch this stuff */}
           <option>Select a source topic</option>
           <option value="Artificial Intelligence">Artificial Intelligence</option>
           <option value="Evolutionary Psychology">Evolutionary Psychology</option>
+          <option value="Python">Python</option>
         </Form.Select>
         <Form.Select
           aria-label="Select source type"
@@ -65,9 +81,11 @@ export default function SourceForm({
           onChange={handleInputChange}
           name="type"
         >
+          {/* TODO fetch this stuff */}
           <option>Select a source type</option>
           <option value="Book">Book</option>
           <option value="Article">Article</option>
+          <option value="Website">Website</option>
         </Form.Select>
       </Form.Group>
       <Button type="submit">{initialData.title ? "Update" : "Submit"}</Button>
