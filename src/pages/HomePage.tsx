b26e7fd995cc7b680c15c6c02eef966edacc4888
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-08-09 08:00:06 +0200

    adds date motd

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,11 +2,22 @@ import { Card, Col, Row } from "react-bootstrap";
 import HomeCardGrid from "../components/HomeCardGrid";
 
 export default function HomePage() {
+  const currentDate = new Date();
+  const formattedCurrentDate = currentDate.toLocaleDateString("en-GB", {
+    weekday: "long",
+    day: "2-digit",
+    month: "2-digit",
+    year: "2-digit",
+  });
+
   return (
     <div className="d-flex flex-column gap-5">
       <Card>
         <Card.Body>
-          <Card.Title className="display-1 fw-semibold">MOTD</Card.Title>
+          <div className="d-flex">
+            <Card.Title className="display-1 fw-semibold">MOTD</Card.Title>
+            <span className="ms-auto">{formattedCurrentDate}</span>
+          </div>
           <Card.Text className="fs-2">
             <span>Love yourself</span>
           </Card.Text>
