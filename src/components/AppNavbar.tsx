e30ead96f393223ff110334228e8a00938de87b4
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-08-09 03:12:14 +0200

    improves navbar

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -1,7 +1,8 @@
+import { NavDropdown } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function AppNavbar() {
   return (
@@ -13,9 +14,26 @@ export default function AppNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to={"/"}>
+            <Nav.Link as={NavLink} to={"/"}>
               Home
             </Nav.Link>
+            <NavDropdown title="Sections" id="basic-nav-dropdown">
+              <NavDropdown.Item as={NavLink} to="/sources">
+                Sources
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/progress">
+                Progress
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/projects">
+                Projects
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/learn">
+                Learn
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/notes">
+                Notes
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
