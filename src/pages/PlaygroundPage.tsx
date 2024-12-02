cfb915c806e2cafcce02e36ed25149687d7167fb
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-12-02 02:40:30 +0100

    re learning shit

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,3 +1,28 @@
+import { Card, Stack } from "react-bootstrap";
+
 export default function PlaygroundPage() {
-  return <p>HELLOOOOOOOO</p>;
+  return (
+    <>
+      <div>
+        <h2 className="mb-4">Playground list title</h2>
+        <Stack gap={3}>
+          <Card className="interactive-card">
+            <Card.Body>
+              <Card.Text>TEST!</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="interactive-card">
+            <Card.Body>
+              <Card.Text>TEST 2!</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="interactive-card">
+            <Card.Body>
+              <Card.Text>TEST 3!</Card.Text>
+            </Card.Body>
+          </Card>
+        </Stack>
+      </div>
+    </>
+  );
 }
