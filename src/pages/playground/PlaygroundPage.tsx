b7c85cfcca86cd94e3ebe33a269c32eccaf228b1
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2024-12-25 05:44:07 +0100

    rename search to clientsearch

--- src/pages/playground/PlaygroundPage.tsx
+++ src/pages/playground/PlaygroundPage.tsx
@@ -92,9 +92,9 @@ const apiTestsReducer = (state: ApiTestsState, action: ApiTestsAction) => {
 export default function PlaygroundPage() {
   const [apiTests, dispatchApiTests] = useReducer(apiTestsReducer, { data: [], isLoading: false, isError: false });
 
-  const [searchTerm, setSearchTerm] = useStorageState("search", "");
+  const [clientSearchTerm, setSearchTerm] = useStorageState("search", "");
 
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleClientSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
@@ -109,23 +109,13 @@ export default function PlaygroundPage() {
   };
 
   const searchedApiTests = apiTests.data.filter((apiTest) =>
-    apiTest.title.toLowerCase().includes(searchTerm.toLowerCase())
+    apiTest.title.toLowerCase().includes(clientSearchTerm.toLowerCase())
   );
-
-  const fetchApiTests = async () => {
-    try {
-      const re = await axios.get(`${API_ENDPOINT}`);
-      dispatchApiTests({ type: APITESTS_FETCH_SUCCESS, payload: re.data });
-    } catch (error) {
-      dispatchApiTests({ type: APITESTS_FETCH_FAILURE });
-    }
-  };
 
   useEffect(() => {
     dispatchApiTests({ type: APITESTS_FETCH_INIT });
-    // fetchApiTests();
-    fetch(`${API_ENDPOINT}`) // B
-      .then((response) => response.json()) // C
+    fetch(`${API_ENDPOINT}`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchApiTests({
           type: "APITESTS_FETCH_SUCCESS",
@@ -140,8 +130,8 @@ export default function PlaygroundPage() {
       <h1>{welcome.greeting}</h1>
       <h2> {welcome.subtitle}</h2>
       <hr />
-      <InputWithLabel id="search" value={searchTerm} onInputChange={handleSearch} isFocused>
-        <strong>Search:</strong>
+      <InputWithLabel id="search" value={clientSearchTerm} onInputChange={handleClientSearch} isFocused>
+        <strong>Client search:</strong>
       </InputWithLabel>
       <hr />
       {apiTests.isError && <p>There was an error!</p>}
