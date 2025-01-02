3776c740db031124565f8b0eb3b3457576bb95aa
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-01-02 10:55:43 +0100

    add css playground section

--- src/pages/playground/CSSPlaygroundPage.tsx
+++ src/pages/playground/CSSPlaygroundPage.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./CSSPlaygroundPage.scss";
 
 export default function CSSPlaygroundPage() {
   return (
-    <div>
-      <h1>CSS Playground</h1>
+    <div className="my-container">
+      <h1 className="my-h1">CSS Playground</h1>
       <p>This is a placeholder page for experimenting with CSS.</p>
     </div>
   );
