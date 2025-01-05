812493dc4730a3649b12fd9b35a19dd0d707e68a
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-01-05 16:40:14 +0100

    generalizes fetch reducer

--- src/pages/playground/api-tests/ApiTestsPlaygroundPage.tsx
+++ src/pages/playground/api-tests/ApiTestsPlaygroundPage.tsx
@@ -4,6 +4,15 @@ import axios from "axios";
 import { useCallback, useEffect, useReducer, useState } from "react";
 import InputWithLabel from "./InputWithLabel";
 import { Button, Form, Spinner } from "react-bootstrap";
+import {
+  DATA_FETCH_FAILURE,
+  DATA_FETCH_INIT,
+  DATA_FETCH_SUCCESS,
+  DataAction,
+  dataReducer,
+  DataState,
+  REMOVE_DATA_ENTRY,
+} from "../../../types/shared/DataState";
 import { ApiTest } from "../../../types/ApiTest";
 
 const API_ENDPOINT = `${import.meta.env.VITE_API_BASE_URL}/api-tests/v1/`;
@@ -23,74 +32,12 @@ const useStorageState = (key: string, initialState: string) => {
   return [value, setValue] as const;
 };
 
-type ApiTestsState = {
-  data: ApiTest[];
-  isLoading: boolean;
-  isError: boolean;
-};
-
-const APITESTS_FETCH_INIT = "APITESTS_FETCH_INIT";
-const APITESTS_FETCH_SUCCESS = "APITESTS_FETCH_SUCCESS";
-const APITESTS_FETCH_FAILURE = "APITESTS_FETCH_FAILURE";
-const REMOVE_APITEST = "REMOVE_APITEST";
-
-type ApiTestsFetchInitAction = {
-  type: typeof APITESTS_FETCH_INIT;
-};
-
-type ApiTestsFetchSuccessAction = {
-  type: typeof APITESTS_FETCH_SUCCESS;
-  payload: ApiTest[];
-};
-
-type ApiTestsFetchFailureAction = {
-  type: typeof APITESTS_FETCH_FAILURE;
-};
-
-type ApiTestsRemoveAction = {
-  type: typeof REMOVE_APITEST;
-  payload: string;
-};
-
-type ApiTestsAction =
-  | ApiTestsFetchInitAction
-  | ApiTestsFetchSuccessAction
-  | ApiTestsFetchFailureAction
-  | ApiTestsRemoveAction;
-
-const apiTestsReducer = (state: ApiTestsState, action: ApiTestsAction) => {
-  switch (action.type) {
-    case APITESTS_FETCH_INIT:
-      return {
-        ...state,
-        isLoading: true,
-        isError: false,
-      };
-    case APITESTS_FETCH_SUCCESS:
-      return {
-        ...state,
-        isLoading: false,
-        isError: false,
-        data: action.payload,
-      };
-    case APITESTS_FETCH_FAILURE:
-      return {
-        ...state,
-        isLoading: false,
-        isError: true,
-      };
-    case REMOVE_APITEST:
-      return {
-        ...state,
-        data: state.data.filter((test) => test.id !== action.payload),
-      };
-    default:
-      throw new Error("Unknown action type");
-  }
-};
-
 export default function ApiTestsPlaygroundPage() {
-  const [apiTests, dispatchApiTests] = useReducer(apiTestsReducer, { data: [], isLoading: false, isError: false });
+  const [apiTests, dispatchApiTests] = useReducer(dataReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
 
   const [clientSearchTerm, setClientSearchTerm] = useStorageState("clientSearch", "");
   const [serverSearchTerm, setServerSearchTerm] = useStorageState("serverSearch", "");
@@ -113,27 +60,28 @@ export default function ApiTestsPlaygroundPage() {
   const handleRemoveApiTest = async (apiTestId: string) => {
     try {
       await axios.delete(`${API_ENDPOINT}${apiTestId}`);
-      dispatchApiTests({ type: REMOVE_APITEST, payload: apiTestId });
+      dispatchApiTests({ type: REMOVE_DATA_ENTRY, payload: apiTestId });
     } catch (error) {
-      dispatchApiTests({ type: APITESTS_FETCH_FAILURE }); // CHECK are we sure about this
+      dispatchApiTests({ type: DATA_FETCH_FAILURE }); // CHECK are we sure about this
       console.error("Failed to delete the API test:", error); // implement clog in reducer?
     }
   };
 
-  const searchedApiTests = apiTests.data.filter((apiTest) =>
-    apiTest.title.toLowerCase().includes(clientSearchTerm.toLowerCase())
-  );
+  // a bit bullshit but only way to remove ts error
+  const searchedApiTests: ApiTest[] = apiTests.data
+    .filter((entry): entry is ApiTest => (entry as ApiTest).title !== undefined)
+    .filter((apiTest) => apiTest.title.toLowerCase().includes(clientSearchTerm.toLowerCase()));
 
   const handleFetchApiTests = useCallback(async () => {
-    dispatchApiTests({ type: APITESTS_FETCH_INIT });
+    dispatchApiTests({ type: DATA_FETCH_INIT });
     try {
       const result = await axios.get(searchUrl);
       dispatchApiTests({
-        type: "APITESTS_FETCH_SUCCESS",
+        type: DATA_FETCH_SUCCESS,
         payload: result.data,
       });
     } catch (error) {
-      dispatchApiTests({ type: "APITESTS_FETCH_FAILURE" });
+      dispatchApiTests({ type: DATA_FETCH_FAILURE });
     }
   }, [searchUrl]);
   //     .then((result) => {
--- src/types/shared/DataState.ts
+++ src/types/shared/DataState.ts
@@ -6,3 +6,66 @@ export type DataState = {
   isLoading: boolean;
   isError: boolean;
 };
+
+export const DATA_FETCH_INIT = "DATA_FETCH_INIT";
+export const DATA_FETCH_FAILURE = "DATA_FETCH_FAILURE";
+export const DATA_FETCH_SUCCESS = "DATA_FETCH_SUCCESS";
+export const REMOVE_DATA_ENTRY = "REMOVE_DATA_ENTRY";
+
+type Payloads = ApiTest[] | GeneralNote[];
+
+type DataFetchInitAction = {
+  type: typeof DATA_FETCH_INIT;
+};
+
+type DataFetchSuccessAction = {
+  type: typeof DATA_FETCH_SUCCESS;
+  payload: Payloads;
+};
+
+type DataFetchFailureAction = {
+  type: typeof DATA_FETCH_FAILURE;
+};
+
+type DataEntryRemoveAction = {
+  type: typeof REMOVE_DATA_ENTRY;
+  payload: string;
+};
+
+export type DataAction = DataFetchInitAction | DataFetchSuccessAction | DataFetchFailureAction | DataEntryRemoveAction;
+
+export const dataReducer = (state: DataState, action: DataAction) => {
+  switch (action.type) {
+    case DATA_FETCH_INIT:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case DATA_FETCH_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case DATA_FETCH_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case REMOVE_DATA_ENTRY:
+      return {
+        ...state,
+        data: state.data.filter((entry) => {
+          if ("id" in entry) {
+            return entry.id !== action.payload;
+          }
+          return true;
+        }) as ApiTest[] | GeneralNote[], // Type assertion to ensure correct type
+      };
+    default:
+      throw new Error("Unknown action type");
+  }
+};
