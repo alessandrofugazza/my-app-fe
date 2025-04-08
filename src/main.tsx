b68a983f01ae43a7f7c0ad0aec76b8f8c88bb939
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-04-08 06:09:44 +0200

    ditto

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import "./index.scss";
 import Root from "./Root.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
-import { Provider } from "react-redux";
-import { store } from "./app/store.ts";
+// import { Provider } from "react-redux";
+// import { store } from "./app/store.ts";
 import PlaygroundHomePage from "./pages/playground/PlaygroundHomePage.tsx";
 import CSSPlaygroundPage from "./pages/playground/css/CSSPlaygroundPage.tsx";
 import ApiTestsPlaygroundPage from "./pages/playground/api-tests/ApiTestsPlaygroundPage.tsx";
@@ -44,10 +44,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      {/* CHECK meh */}
+    {/* <Provider store={store}> */}
+    {/* CHECK meh */}
 
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
+    {/* </Provider> */}
   </React.StrictMode>
 );
