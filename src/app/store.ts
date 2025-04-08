c966d30c967e08ce34899884e90aef19b67fb81b
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-04-08 06:04:04 +0200

    comment redux files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,16 @@
-import type { Action, ThunkAction } from "@reduxjs/toolkit";
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import projectSlice from "../pages/projects/projectSlice";
+// import type { Action, ThunkAction } from "@reduxjs/toolkit";
+// import { combineReducers, configureStore } from "@reduxjs/toolkit";
+// import projectSlice from "../pages/projects/projectSlice";
 
-const rootReducer = combineReducers({
-  project: projectSlice,
-});
+// const rootReducer = combineReducers({
+//   project: projectSlice,
+// });
 
-export const store = configureStore({
-  reducer: rootReducer,
-});
+// export const store = configureStore({
+//   reducer: rootReducer,
+// });
 
-export type AppStore = typeof store;
-export type RootState = ReturnType<AppStore["getState"]>;
-export type AppDispatch = AppStore["dispatch"];
-export type AppThunk<ThunkReturnType = void> = ThunkAction<ThunkReturnType, RootState, unknown, Action>;
+// export type AppStore = typeof store;
+// export type RootState = ReturnType<AppStore["getState"]>;
+// export type AppDispatch = AppStore["dispatch"];
+// export type AppThunk<ThunkReturnType = void> = ThunkAction<ThunkReturnType, RootState, unknown, Action>;
